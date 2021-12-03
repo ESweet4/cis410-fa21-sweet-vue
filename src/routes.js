040745410c45dbd7ab84c907ed1a7a0dcb07e964
Eliana Sweet <esweet4@rams.colostate.edu>
2021-12-02 20:59:06 -0700

    15.2 12/2 header, logout, /account guard

--- src/routes.js
+++ src/routes.js
@@ -9,12 +9,23 @@ import OrderCreate from "./components/OrderCreate.vue";
 import Signup from "./components/Signup.vue";
 import Account from "./components/Account.vue";
 import Items from "./components/Items.vue";
+import myStore from "./store.js";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: Home },
-    { path: "/account", component: Account },
+    {
+      path: "/account",
+      component: Account,
+      beforeEnter(to, from, next) {
+        if (myStore.state.token) {
+          next();
+        } else {
+          next("/login");
+        }
+      },
+    },
     { path: "/login", component: Login },
     { path: "/items", component: Items },
     { path: "/orders", component: Orders },
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { createStore } from "vuex";
 import axios from "axios";
+import myRoutes from "./routes.js";
+
 export default createStore({
   state: {
     token: null,
@@ -16,6 +18,10 @@ export default createStore({
     storeItems(state, items) {
       state.items = items;
     },
+    clearAuthData(state) {
+      state.token = null;
+      state.user = null;
+    },
   },
   actions: {
     getItems({ commit }) {
@@ -23,6 +29,19 @@ export default createStore({
         console.log("response in /items", aResponse);
         commit("storeItems", aResponse.data);
       });
+    },
+    logout({ commit, state }) {
+      axios
+        .post("/contacts/logout", null, {
+          headers: { Authorization: `Bearer ${state.token}` },
+        })
+        .then(() => {
+          commit("clearAuthData");
+          myRoutes.replace("/");
+        })
+        .catch(() => {
+          console.log("error in logging out");
+        });
     },
   },
 });
