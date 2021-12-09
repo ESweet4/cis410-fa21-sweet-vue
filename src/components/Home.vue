f8f1032b00a3aaf74d5d4af05b2833b01d3e81aa
Eliana Sweet <esweet4@rams.colostate.edu>
2021-12-09 16:38:39 -0700

    Finished designing homepage 12/9

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,51 +3,47 @@
     <h1>Home</h1>
   </div>
 
-  <div class="Welcome-message">
-    <h1 id="Welcome">Welcome to The Green Registry</h1>
-    <h2>Your place to find fantastic everyday items!</h2>
-
-    <h3>
-      Please feel free to look at our catalog of items, and create an account
-      when you are ready to order
-    </h3>
-  </div>
-
-  <!-- <div
-    id="carouselExampleControls"
-    class="carousel slide"
-    data-bs-ride="carousel"
-  >
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="./images/couch" class="d-block w-100" alt="Green couch" />
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
+  <div class="my-container-outer Welcome-message">
+    <div class="thisbox">
+      <h2 class="my-container-row" id="Welcome">
+        Welcome to The Green Registry
+      </h2>
+      <h4 class="my-container-row">
+        Your place to find fantastic everyday items!
+      </h4>
+    </div>
+    <div class="card">
+      <img src="../images/couch.jpg" class="card-img-top" alt="A green couch" />
+      <div class="card-body">
+        <p class="card-text">
+          Order this couch today to add it to your registry!
+        </p>
+        <a href="/items" class="btn btn-primary">See Other Items</a>
       </div>
     </div>
-    <button
-      class="carousel-control-prev"
-      type="button"
-      data-bs-target="#carouselExampleControls"
-      data-bs-slide="prev"
-    >
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button
-      class="carousel-control-next"
-      type="button"
-      data-bs-target="#carouselExampleControls"
-      data-bs-slide="next"
-    >
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div> -->
+    <div class="card">
+      <img src="../images/lamp.webp" class="card-img-top" alt="A desk lamp" />
+      <div class="card-body">
+        <p class="card-text">
+          Order this lamp today to complete your office look!
+        </p>
+        <a href="/items" class="btn btn-primary">See Other Items</a>
+      </div>
+    </div>
+
+    <hr />
+    <div class="thisbox my-container-row">
+      <div>
+        <p>
+          <h7>Contact Us: </h7>
+          <br />
+          <h8> Phone: 970-123-4567 </h8>
+          <br />
+          <h8> Email: greenhelp@mail.com</h8>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -55,8 +51,46 @@ export default {};
 </script>
 
 <style>
-.Welcome-message {
-  flex: center;
+.my-container-outer {
+  display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+.my-container-row {
+  padding: 20px;
+  max-width: 1100px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  text-align: center;
+}
+.card {
+  display: flex;
+  flex: wrap;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  width: 18rem;
+  padding: 20px;
+  margin: 20px;
+  background-color: rgba(211, 211, 211, 0.404);
+}
+.thisbox {
+  border: 2px solid rgb(73, 168, 73);
+  border-radius: 25px;
+  padding: 50px;
+  margin-bottom: 10px;
+}
+h7 {
+  font-weight: bold;
+}
+hr {
+  width: 90%;
+  height: 1px;
+  text-align: center;
+  background-color: rgb(8, 0, 0);
+  size: 10px;
 }
 </style>
